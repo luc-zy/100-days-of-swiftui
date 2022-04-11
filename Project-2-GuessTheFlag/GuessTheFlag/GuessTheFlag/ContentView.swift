e677e24e7ec5f6b3d280e68330726dd0a6e08f5a
zhangyang <yzhang_ws@outlook.com>
2022-04-11 09:21:10 +0800

    添加guessTheFlag的demo代码

--- Project-2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project-2-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+
+    var correctAnswer = Int.random(in: 0...2)
+    
+    @State var scoreTitle = ""
+    @State var showingScore = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag").font(.title)
+                    Text(countries[correctAnswer]).font(.headline)
+                }.foregroundColor(.white)
+                ForEach(0..<3) { number in
+                    Button {
+                        if(number == correctAnswer) {
+                            scoreTitle = "Correct"
+                        }else{
+                            scoreTitle = "Wrong"
+                        }
+                        showingScore = true
+                    } label: {
+                        Image(countries[number]).renderingMode(.original)
+                    }.alert(scoreTitle, isPresented: $showingScore) {
+                        Button("OK", role: .cancel) {
+                            
+                        }
+                    }
+
+                }
+            }
+        }
     }
 }
 
