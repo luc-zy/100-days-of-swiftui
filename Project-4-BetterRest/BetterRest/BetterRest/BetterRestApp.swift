96b3b3858d2a289896daafe201f847b42d85e90d
zhangyang <yzhang_ws@outlook.com>
2022-06-08 09:32:05 +0800

    betterRest

---
+++ Project-4-BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 张洋 on 2022/5/30.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
